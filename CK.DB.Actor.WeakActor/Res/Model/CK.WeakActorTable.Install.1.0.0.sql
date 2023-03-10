f9c1a7b79bedd2bd4299e897cf826b443fa5cf12
Aymeric Richard <aymeric.richard@signature-code.com>
2023-03-10 17:10:07 +0100

    Add CreationDate to CK.tWeakActor.

--- CK.DB.Actor.WeakActor/Res/Model/CK.WeakActorTable.Install.1.0.0.sql
+++ CK.DB.Actor.WeakActor/Res/Model/CK.WeakActorTable.Install.1.0.0.sql
@@ -6,8 +6,15 @@ create table CK.tWeakActor
 	    constraint PK_CK_tWeakActor primary key nonclustered ( WeakActorId )
         constraint FK_CK_tWeakActor_WeakActorId foreign key references CK.tActor( ActorId ),
 
+    -- Collation should be Case insensitive at least (this is the recommended practice for user-like names).
+    -- 255 seems large but this is to support emails as user-like names: emails can be 254 unicode characters long.
     WeakActorName nvarchar( 255 ) collate Latin1_General_100_CI_AS not null
-        constraint UK_CK_tWeakActor_WeakActorName unique
+        constraint UK_CK_tWeakActor_WeakActorName unique,
+
+    -- Overall storage size for datetime2(0) is the same as for datetime2(2): 7 bytes.
+    -- Let's keep the better precision for it.
+    CreationDate datetime2 (2) not null
+        constraint DF_CK_tWeakActor_CreationDate default ( sysutcdatetime() ),
 );
 
 insert into CK.tWeakActor( WeakActorId, WeakActorName ) values ( 0, N'' );
