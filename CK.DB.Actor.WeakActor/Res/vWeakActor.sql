95024bdc0d0757886979ea2ab9237cc136566dbd
Aymeric Richard <aymeric.richard@signature-code.com>
2023-03-21 18:08:53 +0100

    Use an alias to support join table in a future potential tql.

--- CK.DB.Actor.WeakActor/Res/vWeakActor.sql
+++ CK.DB.Actor.WeakActor/Res/vWeakActor.sql
@@ -1,6 +1,7 @@
 create view CK.vWeakActor
 as
-    select WeakActorId,
-           WeakActorName,
-           CreationDate
-    from CK.tWeakActor;
+    select w.WeakActorId,
+           w.WeakActorName,
+           w.CreationDate,
+           DisplayName = w.WeakActorName
+    from CK.tWeakActor w;
