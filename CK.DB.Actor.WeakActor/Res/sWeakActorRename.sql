0cf4a32dae048a924e70b4724d2b222689d491e0
Dan Chiche <dan.chiche@signature.one>
2025-11-07 13:46:04 +0100

    Fix space between tql inject

--- CK.DB.Actor.WeakActor/Res/sWeakActorRename.sql
+++ CK.DB.Actor.WeakActor/Res/sWeakActorRename.sql
@@ -9,19 +9,19 @@ begin
     if @ActorId <= 0 throw 50000, 'Security.AnonymousNotAllowed', 1;
     if @WeakActorId <= 0 throw 50000, 'WeakActor.InvalidWeakActorId', 1;
 
-    -- <PostArgumentsCheck />
+    --<PostArgumentsCheck />
 
     if exists( select 1 from CK.tWeakActor where WeakActorId = @WeakActorId )
     begin
         if exists( select 1 from CK.tWeakActor where WeakActorId <> @WeakActorId and WeakActorName = @WeakActorName )
             throw 50000, 'WeakActor.NameAlreadyTaken', 1;
 
-        -- <PreUpdate />
+        --<PreUpdate />
 
         update CK.tWeakActor
         set WeakActorName = @WeakActorName
         where WeakActorId = @WeakActorId;
 
-        -- <PostUpdate />
+        --<PostUpdate />
     end
 end
